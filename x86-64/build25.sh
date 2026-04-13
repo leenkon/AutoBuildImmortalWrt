628bef0e791aef5a4cac9030990d9a7dd2f5db1b
leenkon <leenkon@qq.com>
2026-04-13 14:55:19 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 # Log file for debugging
-# 目前暂不支持第三方软件apk 待后续开发 仓库内可以集成
-source shell/custom-packages.sh
-#echo "第三方软件包: $CUSTOM_PACKAGES"
+# 目前支持少部分第三方软件apk 通过打开shell/apk-custom-packages.sh的注释来集成
+source shell/apk-custom-packages.sh
+echo "第三方apk软件包: $CUSTOM_PACKAGES"
 LOGFILE="/tmp/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 echo "编译固件大小为: $PROFILE MB"
 echo "Include Docker: $INCLUDE_DOCKER"
 
-# 创建自定义设置脚本，直接设置 UCI 配置
-cat << 'EOF' > /home/build/immortalwrt/files/etc/custom-settings.sh
-#!/bin/sh
-[ -n "${CUSTOM_ROUTER_IP}" ] && uci set network.lan.ipaddr="${CUSTOM_ROUTER_IP}"
+echo "Create pppoe-settings"
+mkdir -p  /home/build/immortalwrt/files/etc/config
 
-[ -n "${CUSTOM_HOSTNAME_VAL}" ] && uci set system.@system[0].hostname="${CUSTOM_HOSTNAME_VAL}"
-
-# PPPoE settings
-[ "${ENABLE_PPPOE}" = "yes" ] && [ -n "${PPPOE_ACCOUNT}" ] && [ -n "${PPPOE_PASSWORD}" ] && {
-    uci set network.wan=interface
-    uci set network.wan.proto='pppoe'
-    uci set network.wan.username="${PPPOE_ACCOUNT}"
-    uci set network.wan.password="${PPPOE_PASSWORD}"
-    uci set network.wan.peerdns='1'
-    uci set network.wan.auto='1'
-    uci set network.wan6.proto='none'
-}
-
-uci commit
+# 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
+cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
+enable_pppoe=${ENABLE_PPPOE}
+pppoe_account=${PPPOE_ACCOUNT}
+pppoe_password=${PPPOE_PASSWORD}
 EOF
 
-chmod +x /home/build/immortalwrt/files/etc/custom-settings.sh
+echo "cat pppoe-settings"
+cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 
-# 替换环境变量
-sed -i "s|\${CUSTOM_ROUTER_IP}|${CUSTOM_ROUTER_IP}|g; s|\${CUSTOM_HOSTNAME_VAL}|${CUSTOM_HOSTNAME_VAL}|g; s|\${ENABLE_PPPOE}|${ENABLE_PPPOE}|g; s|\${PPPOE_ACCOUNT}|${PPPOE_ACCOUNT}|g; s|\${PPPOE_PASSWORD}|${PPPOE_PASSWORD}|g" /home/build/immortalwrt/files/etc/custom-settings.sh
+if [ -z "$CUSTOM_PACKAGES" ]; then
+  echo "⚪️ 未选择 任何第三方软件包"
+else
+  # ============= 同步第三方插件库==============
+  # 同步第三方软件仓库run/apk
+  echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
+  git clone --depth=1 https://github.com/wukongdaily/apk.git /tmp/store-apk-repo
+
+  # 拷贝 run/x86 下所有 run 文件和apk文件 到 extra-packages 目录
+  mkdir -p /home/build/immortalwrt/extra-packages
+  cp -r /tmp/store-apk-repo/run/x86/* /home/build/immortalwrt/extra-packages/
+
+  echo "✅ Run files copied to extra-packages:"
+  ls -lh /home/build/immortalwrt/extra-packages/*.run
+  # 解压并拷贝apk到packages目录
+  sh shell/apk-prepare-packages.sh
+  ls -lah /home/build/immortalwrt/packages/
+fi
+
 
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
@@ -41,19 +48,24 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-#24.10
+#25.12
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES xray-core hysteria luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
 
-# ======== shell/custom-packages.sh =======
+# 文件管理器
+PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+# ======== shell/apk-custom-packages.sh =======
 # 合并imm仓库以外的第三方插件 暂时注释
-#PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
+PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 
 # 判断是否需要编译 Docker 插件
